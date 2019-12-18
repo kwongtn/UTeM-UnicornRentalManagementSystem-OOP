aebaa641ba8f54d426e1fb559ac68b30efa1251a
KwongTN <tungnan5636@gmail.com>
2019-12-19 00:56:58 +0800

    Update Unicorn.java
    Removed some comments

--- src/model/Unicorn.java
+++ src/model/Unicorn.java
@@ -24,72 +24,42 @@ public class Unicorn extends Model{
         this.name = name;
     }
 
-    /**
-     * @return the type
-     */
     public String getType() {
         return type;
     }
 
-    /**
-     * @param type the type to set
-     */
     public void setType(String type) {
         this.type = type;
     }
 
-    /**
-     * @return the rate
-     */
     public double getRate() {
         return rate;
     }
 
-    /**
-     * @param rate the rate to set
-     */
     public void setRate(double rate) {
         this.rate = rate;
     }
 
-    /**
-     * @return Unicorn color
-     */
     public String getColor() {
         return color;
     }
 
-    /**
-     * @param color Unicorn Color
-     */
     public void setColor(String color) {
         this.color = color;
     }
 
-    /**
-     * @return the available
-     */
     public boolean isAvailable() {
         return available;
     }
 
-    /**
-     * @param available the available to set
-     */
     public void setAvailable(boolean available) {
         this.available = available;
     }
 
-    /**
-     * @return the healthCheck
-     */
     public boolean isHealthCheck() {
         return healthCheck;
     }
 
-    /**
-     * @param healthCheck the healthCheck to set
-     */
     public void setHealthCheck(boolean healthCheck) {
         this.healthCheck = healthCheck;
     }

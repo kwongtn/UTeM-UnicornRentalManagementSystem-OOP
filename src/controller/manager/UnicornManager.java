4c69e3b3377b9cd64782c40e4b6b0f34a05eb20d
KwongTN <tungnan5636@gmail.com>
2019-12-19 01:07:54 +0800

    Added update unicorn function for database

--- src/controller/manager/UnicornManager.java
+++ src/controller/manager/UnicornManager.java
@@ -8,7 +8,8 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 import model.Unicorn;
-public class UnicornManager{
+
+public class UnicornManager {
 
 	private static Vector<Unicorn> unicorns = new Vector<>();
 
@@ -68,26 +69,56 @@ public class UnicornManager{
 
 	}
 
-	public static Vector<Unicorn> getUnicorns() throws ClassNotFoundException, SQLException{
+	public static int updateUnicorn(Unicorn unicorn) throws ClassNotFoundException, SQLException{
+		Class.forName("com.mysql.jdbc.Driver");
+
+		Connection connection = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+		PreparedStatement ps = connection.prepareStatement("UPDATE UNICORN SET name=?, type=?, rate=?, color=?, availability=?, healthCheck=? WHERE unicornID=?");
+
+		ps.setString(1, unicorn.getName());
+		ps.setString(2, unicorn.getType());
+		ps.setDouble(3, unicorn.getRate());
+		ps.setString(4, unicorn.getColor());
+		ps.setBoolean(5, unicorn.isAvailable());
+		ps.setBoolean(6, unicorn.isHealthCheck());
+		ps.setInt(7, unicorn.getUnicornID());
+
+		int status = ps.executeUpdate();
+
+		
+		int index = -1;
+		for (int i = 0; i < unicorns.size(); i++) {
+			Unicorn temp = unicorns.get(i);
+			if (temp != null && (temp.getUnicornID() == unicorn.getUnicornID())) {
+				unicorns.set(index, unicorn);
+				index = i;
+				break;
+			}
+		}
+
+		return status;
+	}
+
+	public static Vector<Unicorn> getUnicorns() throws ClassNotFoundException, SQLException {
 
 		Class.forName("com.mysql.jdbc.Driver");
 
 		Connection connection = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
-		PreparedStatement ps = connection.prepareStatement("SELECT name, type, rate, color, available, healthCheck FROM Unicorn");
+		PreparedStatement ps = connection.prepareStatement("SELECT * FROM UNICORN");
 		ResultSet rs = ps.executeQuery();
 
 		Vector<Unicorn> unicorns = new Vector<>();
 
-		while(rs.next()){
+		while (rs.next()) {
 			Unicorn unicorn = new Unicorn();
 
-			unicorn.setName(rs.getString(1));
-			unicorn.setType(rs.getString(2));
-			unicorn.setRate(rs.getDouble(3));
-			unicorn.setColor(rs.getString(4));
-			unicorn.setAvailable(rs.getBoolean(5));
-			unicorn.setHealthCheck(rs.getBoolean(6));
-
+			unicorn.setUnicornID(rs.getInt(1));
+			unicorn.setName(rs.getString(2));
+			unicorn.setType(rs.getString(3));
+			unicorn.setRate(rs.getDouble(4));
+			unicorn.setColor(rs.getString(5));
+			unicorn.setAvailable(rs.getBoolean(6));
+			unicorn.setHealthCheck(rs.getBoolean(7));
 
 			unicorns.add(unicorn);
 		}
@@ -96,6 +127,5 @@ public class UnicornManager{
 
 		return unicorns;
 	}
-	
-	
+
 }

6267d259f10ef6f9b9a6fc1c42134e4e07d104f7
KwongTN <tungnan5636@gmail.com>
2019-12-19 11:12:02 +0800

    Rely db connection variables to class dbManager

---
+++ src/controller/manager/dbManager.java
@@ -0,0 +1,7 @@
+package controller.manager;
+
+public class dbManager{
+    protected static String dbAddress = "jdbc:mysql://localhost:8080/UNICORNDB";
+	protected static String dbUsername = "root";
+	protected static String dbPassword = "password";
+}

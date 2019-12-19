8e4f19f88b02089cece381c53d44f1f9f4c73a1f
KwongTN <tungnan5636@gmail.com>
2019-12-19 13:56:23 +0800

    Complete basic functions for RentalManager.java

--- src/controller/manager/RentalManager.java
+++ src/controller/manager/RentalManager.java
@@ -14,6 +14,90 @@ public class RentalManager extends dbManager{
 
 	private static Vector<Rental> rentals = new Vector<>();
 
+	public static int addRental(Rental rental) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+
+		Connection connection = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+		PreparedStatement ps = connection.prepareStatement("INSERT INTO RENTAL(unicornID, customerID, start, end, depositPaid) VALUES (?,?,?,?,?)");
+		
+        ps.setInt(1, rental.getUnicornID());
+        ps.setInt(2, rental.getCustomerID());
+        ps.setLong(3, rental.getStartDate());
+        ps.setLong(4, rental.getEndDate());
+        ps.setDouble(5, rental.getDepositPaid());
+
+		int status = ps.executeUpdate();
+
+		rental.setRentalID(connection.prepareStatement("SELECT MAX(rentalID) FROM RENTAL").executeQuery().getInt(1));
+
+		connection.close();
+
+		return status;
+	}
+
+	public static int deleteRental(int rentalID) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+
+		Connection connection = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+		PreparedStatement ps = connection.prepareStatement("DELETE FROM RENTAL WHERE rentalID=?");
+
+		ps.setInt(1, rentalID);
+
+		int status = ps.executeUpdate();
+
+		connection.close();
+
+		int index = -1;
+		for(int i = 0; i < rentals.size(); i++){
+			Rental temp = rentals.get(i);
+
+			if(temp != null && (temp.getRentalID() == rentalID)){
+				index = i;
+
+				break;
+			}
+		}
+
+		try {
+			rentals.remove(index);
+			return status;
+		} catch (Exception e) {
+			return 1 + status;
+		}
+
+	}
+
+	public static int updateRental(Rental rental) throws ClassNotFoundException, SQLException{
+		Class.forName("com.mysql.jdbc.Driver");
+
+		Connection connection = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+		PreparedStatement ps = connection.prepareStatement("UPDATE RENTAL SET rentalID=?, unicornID=?, customerID=?, start=?, end=?, depositPaid=? WHERE rentalID=?");
+
+        ps.setInt(1, rental.getRentalID());
+        ps.setInt(2, rental.getUnicornID());
+        ps.setInt(3, rental.getCustomerID());
+        ps.setLong(4, rental.getStartDate());
+        ps.setLong(5, rental.getEndDate());
+        ps.setDouble(6, rental.getDepositPaid());
+        ps.setInt(7, rental.getRentalID());
+
+		int status = ps.executeUpdate();
+
+		
+		int index = -1;
+		for (int i = 0; i < rentals.size(); i++) {
+			Rental temp = rentals.get(i);
+			if (temp != null && (temp.getRentalID() == rental.getRentalID())) {
+				rentals.set(index, rental);
+				index = i;
+				break;
+			}
+		}
+
+		return status;
+	}
+
+	public static Vector<Rental> getRentals() throws ClassNotFoundException, SQLException {
 
 		Class.forName("com.mysql.jdbc.Driver");
 
@@ -56,5 +140,19 @@ public class RentalManager extends dbManager{
 		
 		return null;
 	}
+
+	public static Rental getCurrentRentalByUnicornID(int unicornID) throws ClassNotFoundException, SQLException {
+		Vector<Rental> rentals = getRentals();
+
+		for(int i = 0; i < rentals.size(); i++){
+			Rental rental = rentals.get(i);
+			if((rental.getUnicornID() == unicornID) && !rental.isReturned()){
+				return rental;
+			}
+		}
+		
+		return null;
+	}
+
 
 }
